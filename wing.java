60632d738750e8265f0b408236d72c3fc3cf3da4
pankaj10664 <pankaj10664@gmail.com>
2018-06-14 11:38:21 +0530

    string to int

---
+++ wing.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Sion
+{ 
+
+public static void main(String args[])
+{
+	String x="200";
+String y="400";
+int a = Integer.parseInt(x);
+int b = Integer.parseInt(y);
+System.out.println("the value of a is="+a);
+System.out.println("the value of b is="+b);
+if(a>b)
+{
+	System.out.println("a is maximum");
+
+}
+else
+{
+	System.out.println("b is maximum");
+
+}
+}
+}
